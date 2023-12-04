dea8914a9cae43047d9d296ecc1865cc80dc74b6
Andrew <49104171+carsonak@users.noreply.github.com>
2023-12-04 20:57:46 +0300

    switched to format str

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 for ltr in "zyxwvutsrqponmlkjihgfedcba":
-    print(f"{chr(ord(ltr) -32) if ord(ltr) % 2 else ltr}", end="")
+    print("{}".format(chr(ord(ltr) - 32) if ord(ltr) % 2 else ltr), end="")
