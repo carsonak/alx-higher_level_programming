b5a98fde7ebbb76df58a5bc8a890e86ef9385eb2
Andrew <49104171+carsonak@users.noreply.github.com>
2023-12-07 13:56:59 +0300

    Wven shorter

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,4 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    if matrix and type(matrix) is list:
-        return [list(map(lambda iL: list(map((lambda x: x**2), iL)), matrix))]
+    return [list(map(lambda iL: list(map((lambda x: x**2), iL)), matrix))]
