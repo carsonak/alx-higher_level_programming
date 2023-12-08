19017811c6cf4759fd2335b4faba85bc51801666
Andrew <49104171+carsonak@users.noreply.github.com>
2023-12-08 23:18:31 +0300

    Changed file permissions

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -2,6 +2,7 @@
 import random
 number = random.randint(-10000, 10000)
 lst_dgt = (number % 10) if number >= 0 else -(abs(number) % 10)
+
 print(f"Last digit of {number:d} is ", end="")
 if lst_dgt > 5:
     print(f"{lst_dgt} and is greater than 5")
