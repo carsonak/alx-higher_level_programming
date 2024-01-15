22d6d38f01296ae0ce05ec1fc491b9f4f11058d6
Andrew <49104171+carsonak@users.noreply.github.com>
2024-01-16 01:02:19 +0300

    Added readme

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Square"""
 
 class Square:
-    """"""
+    """Square"""
 
     def __init__(self, size=0):
         """Initialise size and raise errors"""
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
+"""Square"""
 
 class Square:
-    """"""
+    """Square"""
 
     def __init__(self, size=0):
         self.__size = size
