7768d36400067874f356a201a1b2d5c127b50202
Andrew <49104171+carsonak@users.noreply.github.com>
2024-02-12 10:42:05 +0300

    corrections

--- 0x0B-python-input_output/101-stats.py
+++ 0x0B-python-input_output/101-stats.py
@@ -5,3 +5,7 @@ import sys
 
 def logger():
     """"""
+
+
+if __name__ == "__main__":
+    logger()
---
+++ mk_num_files.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+common_name="-answer.txt"  # Replace the string in qoutes with your desired common name
+is_suffix=0  # 0 for prefix, 1 for suffix
+
+start_at=23   # Set the starting number for the files
+stop_at=28   # Set the end of the files
+step_size=1	# Set the step size
+
+for i in $(eval echo "{$start_at..$stop_at..$step_size}")
+do
+	if [[ $is_suffix -eq 0 ]]; then
+		touch "$i$common_name"
+	else
+		touch "$common_name$i"
+	fi
+done
