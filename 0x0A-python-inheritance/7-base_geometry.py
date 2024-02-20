4a372ff213d1bd0b47c64f01b58197777a6ce0a0
Andrew <49104171+carsonak@users.noreply.github.com>
2024-02-20 22:16:41 +0300

    Added 6-8

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry"""
+
+
+class BaseGeometry():
+    """BaseGeometry class"""
+
+    def area(self):
+        """Work in progress"""
+
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """Validates value and type of value"""
+
+        if type(value) is not int:
+            raise TypeError(f"{name} must be an integer")
+
+        if value < 1:
+            raise ValueError(f"{name} must be greater than 0")
+
+
+if __name__ == "__main__":
+
+    bg = BaseGeometry()
+
+    bg.integer_validator("my_int", 12)
+    bg.integer_validator("width", 89)
+
+    try:
+        bg.integer_validator("name", "John")
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        bg.integer_validator("age", 0)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
+
+    try:
+        bg.integer_validator("distance", -4)
+    except Exception as e:
+        print("[{}] {}".format(e.__class__.__name__, e))
---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for Rectangle"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Rectangle class"""
