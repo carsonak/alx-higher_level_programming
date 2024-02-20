a3c18bb8b8584381ab68ed81fdf7f4da2ecd534f
Andrew <49104171+carsonak@users.noreply.github.com>
2024-02-20 21:18:33 +0300

    Added 0-5

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+"""lookup module"""
+
+
+def lookup(obj):
+    """Return a list of an object's attributes and methods"""
+
+    return dir(obj)
+
+
+if __name__ == "__main__":
+    class MyClass1(object):
+        pass
+
+    class MyClass2(object):
+        my_attr1 = 3
+
+        def my_meth(self):
+            pass
+
+    print(lookup(MyClass1))
+    print(lookup(MyClass2))
+    print(lookup(int))
---
+++ 0x0A-python-inheritance/5-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module for BaseGeometry"""
+
+
+class BaseGeometry():
+    """BaseGeometry class"""
+
+    pass
+
+
+if __name__ == "__main__":
+
+    bg = BaseGeometry()
+
+    print(bg)
+    print(dir(bg))
+    print(dir(BaseGeometry))
