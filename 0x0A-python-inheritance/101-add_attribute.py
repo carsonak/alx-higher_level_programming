17f26a7893354f81362685afcf2d5a8da2872188
Andrew <49104171+carsonak@users.noreply.github.com>
2024-02-21 15:46:32 +0300

    removed try catch block

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,23 +5,7 @@
 def add_attribute(obj, attr_name, attr_val):
     """Add an attribute to an object if possible otherwise raise TypeError"""
 
-    if "__dict__" in dir(obj) and type(obj.__dict__) is dict:
+    if hasattr(obj, "__dict__") and type(obj.__dict__) is dict:
         obj.__dict__[str(attr_name)] = attr_val
     else:
         raise TypeError("can't add new attribute")
-
-
-if __name__ == "__main__":
-    class MyClass():
-        pass
-
-    mc = MyClass()
-    add_attribute(mc, "name", "John")
-    print(mc.name)
-
-    try:
-        a = "My String"
-        add_attribute(a, "name", "Bob")
-        print(a.name)
-    except Exception as e:
-        print("[{}] {}".format(e.__class__.__name__, e))
