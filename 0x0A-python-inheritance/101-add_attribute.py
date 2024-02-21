9041168f65e9558383edec847946ff04bc030441
Andrew <49104171+carsonak@users.noreply.github.com>
2024-02-21 15:45:14 +0300

    removed hasattr

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -5,7 +5,7 @@
 def add_attribute(obj, attr_name, attr_val):
     """Add an attribute to an object if possible otherwise raise TypeError"""
 
-    if hasattr(obj, "__dict__") and type(obj.__dict__) is dict:
+    if "__dict__" in dir(obj) and type(obj.__dict__) is dict:
         obj.__dict__[str(attr_name)] = attr_val
     else:
         raise TypeError("can't add new attribute")
