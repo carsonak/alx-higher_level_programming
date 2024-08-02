7d812c56065db63c5288482145a12edc318c2836
Andrew <49104171+carsonak@users.noreply.github.com>
2024-08-02 11:53:50 +0300

    0x0D-SQL_introduction

--- py_header.sh
+++ py_header.sh
@@ -1,4 +1,23 @@
 #!/usr/bin/env bash
-for file in $(find . -name "*.py" -type f)
-do echo '#!/usr/bin/python3' > $file && echo '"""Module for '"$(basename -s .py $file)"'."""' >> $file
+# Add a shebang and a simple docstring to all python files in the workspace
+
+work_files=$(find . -name "*.py" -type f)
+
+
+for file in $work_files
+do
+    if [[ -s "$file" ]]
+    then
+        if head -1 "$file" | grep -qvE '^#!'
+        then
+            sed -i -e '1i\#!/usr/bin/python3' \
+                -e "1i\"\"\"Module for $(basename -s .py $file).\"\"\"" \
+                "$file"
+        fi
+    else
+        echo '#!/usr/bin/python3' > "$file" &&
+        echo '"""Module for '"$(basename -s .py $file)"'."""' >> "$file"
+    fi
+
+    chmod u+x "$file"
 done
